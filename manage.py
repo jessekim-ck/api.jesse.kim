733ea1029c582dd5db9e765d4a279f6423e6754c
jessekim <jessekim.ck.94@gmail.com>
2019-09-04 23:32:02 +0900

    Settings for deploy

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weekendkim_backend.settings.debug')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weekendkim_backend.settings.deploy')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
