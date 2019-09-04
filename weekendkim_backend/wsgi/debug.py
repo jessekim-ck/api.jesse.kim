0edd1b4304ff4a9e72aef0a16819923949a72c2e
jessekim <jessekim.ck.94@gmail.com>
2019-09-04 23:54:03 +0900

    Settings for deploy

--- weekendkim_backend/wsgi/debug.py
+++ weekendkim_backend/wsgi/debug.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.debug")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weekendkim_backend.settings.debug")
 application = get_wsgi_application()
 
