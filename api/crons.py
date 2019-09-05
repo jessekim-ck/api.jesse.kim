9ccdae0b7a261e3d0df078d2731416c9af8165fa
jessekim <jessekim.ck.94@gmail.com>
2019-09-05 12:17:52 +0900

    Foreignkey argument should be a object

--- api/crons.py
+++ api/crons.py
@@ -2,6 +2,6 @@ from .models import Post
 
 
 def test_cron_job():
-    Post.objects.create(title='Cron Test Title', text="Cron Test Text")
+    Post.objects.create(title='Cron Test Title', text="Cron Test Text", writer_id_id=1)
     pass
 
